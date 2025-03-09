ce24aadb53d33bd6170acf8d8939677f62a2aaea
Salwa Nafisa <111229380+Salwajysn@users.noreply.github.com>
2025-03-09 22:18:52 +0700

    Add files via upload

---
+++ CRUD/app3.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const userRoutes = require('./routes/claimRoutes');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware
+app.use(bodyParser.json());
+
+// Routing
+app.use(userRoutes);
+
+// Menjalankan server
+app.listen(port, () => {
+ console.log(`Server berjalan pada port ${port}`);
+});
